d636ce4d8af726619f955cbb406a163bd3fa1381
Ventsi Nurkov <ventsislav.nurkov@gmail.com>
2022-06-04 16:54:44 +0300

    Modified: make_symlinks.sh

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -3,30 +3,30 @@
 # get user homedir
 # HOMEDIR=$( getent passwd "$USER" | cut -d: -f6 )
 HOMEDIR=$(eval echo "~$USER")
-OS_BASH_FILE=$HOMEDIR/.bashrc
+OS_BASH_FILE=$HOMEDIR/.bashrc 
 OS_VIM_FILE=$HOMEDIR/.vimrc
 OS_VIM_DIR=$HOMEDIR/.vim
 
 if [ -f "$OS_BASH_FILE" ]; then
     echo "$OS_BASH_FILE exists, replacing"
-    rm -f $OS_BASH_FILE
+    unlink $OS_BASH_FILE
 fi
 
 if [ -f "$OS_VIM_FILE" ]; then
     echo "$OS_VIM_FILE exists, replacing"
-    rm -f $OS_VIM_FILE
+    unlink $OS_VIM_FILE
 fi
 
-if [ -f "$OS_VIM_DIR" ]; then
+if [ -d "$OS_VIM_DIR" ]; then
     echo "$OS_VIM_DIR exists, replacing"
-    rm -rf $OS_VIM_DIR
+    unlink $OS_VIM_DIR
 fi
 
 # make bashrc symlink
-ln -sf $PWD/.bashrc $HOMEDIR/.bashrc && echo "bashrc link created"
+ln -sf $PWD/.bashrc $OS_BASH_FILE && echo "bashrc link created"
 
 # make vimrc symlink
-ln -sf $PWD/.vimrc $HOMEDIR/.vimrc && echo "vimrc link created"
+ln -sf $PWD/.vimrc $OS_VIM_FILE && echo "vimrc link created"
 
 # make .vim folder symlink
-ln -sf $PWD/.vim $HOMEDIR/.vim && echo "vim folder link created"
+ln -sf $PWD/.vim $OS_VIM_DIR && echo "vim folder link created"
