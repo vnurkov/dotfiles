adc5ecdb0a1f0c2e54ac202da8b926a0e8307a84
Ventsi Nurkov <ventsislav.nurkov@gmail.com>
2022-06-07 20:39:50 +0300

    modified: make_symlinks.sh

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -2,31 +2,46 @@
 
 # get user homedir
 # HOMEDIR=$( getent passwd "$USER" | cut -d: -f6 )
-HOMEDIR=$(eval echo "~$USER")
+HOMEDIR=$(eval printf "~$USER")
 OS_BASH_FILE=$HOMEDIR/.bashrc 
 OS_VIM_FILE=$HOMEDIR/.vimrc
 OS_VIM_DIR=$HOMEDIR/.vim
 
 if [ -f "$OS_BASH_FILE" ]; then
-    echo "$OS_BASH_FILE exists, replacing"
-    readlink $OS_BASH_FILE && unlink $OS_BASH_FILE && echo "Unlinked symlink" || rm -f $OS_BASH_FILE && echo "Removed file"
+    if readlink -q "$OS_BASH_FILE"; then
+        printf "bashrc symlink found, unlinking...\n"
+        unlink $OS_BASH_FILE && printf "$OS_BASH_FILE Unlinked!\n\n"
+    else
+        printf "bashrc file found, deleting...\n"
+        rm -f $OS_BASH_FILE && printf "$OS_BASH_FILE Deleted!\n\n"
+    fi
 fi
 
 if [ -f "$OS_VIM_FILE" ]; then
-    echo "$OS_VIM_FILE exists, replacing"
-    unlink $OS_VIM_FILE
+    if readlink "$OS_VIM_FILE"; then
+        printf "vimrc symlink found, unlinking...\n"
+        unlink $OS_VIM_FILE && printf "$OS_VIM_FILE Unlinked!\n\n"
+    else
+        printf "vimrc file found, deleting...\n"
+        rm -f $OS_VIM_FILE && printf "$OS_VIM_FILE Deleted!\n\n"
+    fi
 fi
 
 if [ -d "$OS_VIM_DIR" ]; then
-    echo "$OS_VIM_DIR exists, replacing"
-    unlink $OS_VIM_DIR
+    if readlink "$OS_VIM_DIR"; then
+        printf "vim folder symlink found, unlinking...\n"
+        unlink $OS_VIM_DIR && printf "$OS_VIM_DIR Unlinked!\n\n"
+    else
+        printf "vim folder found, deleting...\n"
+        rm -rf $OS_VIM_DIR && printf "$OS_VIM_DIR Deleted!\n\n"
+    fi
 fi
 
 # make bashrc symlink
-ln -sf $PWD/.bashrc $OS_BASH_FILE && echo "bashrc link created"
+ln -sf $PWD/.bashrc $OS_BASH_FILE && printf "bashrc link created\n"
 
 # make vimrc symlink
-ln -sf $PWD/.vimrc $OS_VIM_FILE && echo "vimrc link created"
+ln -sf $PWD/.vimrc $OS_VIM_FILE && printf "vimrc link created\n"
 
 # make .vim folder symlink
-ln -sf $PWD/.vim $OS_VIM_DIR && echo "vim folder link created"
+ln -sf $PWD/.vim $OS_VIM_DIR && printf "vim folder link created\n"
